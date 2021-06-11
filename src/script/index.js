b75fbee27e0fcd542866087bddb9343afc3f94f8
Camillodo <camille.doizelet@gmail.com>
2021-06-11 18:06:16 +0200

    html css done

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+const cards = document.querySelectorAll('.memory_card');
+
+function flipCard (){
+this.classList.toggle('flipped')
+}
+
+
+cards.forEach(card => {card.addEventListener('click', flipCard)})
